9a2bd670eeaeb6263e7deb129acd6c114a190ff2
dirac <78232235+diracduran@users.noreply.github.com>
2022-04-13 12:23:43 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDtIin-3s20LnsAc-J3J6xpKgBhQcvI9zY",
-    authDomain: "whatsappnextjs-efaee.firebaseapp.com",
-    projectId: "whatsappnextjs-efaee",
-    storageBucket: "whatsappnextjs-efaee.appspot.com",
-    messagingSenderId: "255446595166",
-    appId: "1:255446595166:web:2c78021154af3de994e6cf"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
   };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
